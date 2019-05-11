b66a4eab4b83e8f699f62785e5d6f5c1cf154db2
DESKTOP-RV5IBVV\aLEX <postualexandru@yahoo.com>
2019-05-12 00:25:28 +0300

    no message

---
+++ Game Jam Project/Assets/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public List<Vector3> v = new List<Vector3>();
+    public float carSpeed = 1000000;
+    private float time = 10f;
+
+    void Update()
+    {
+        MovePlayer();
+        checkTime();
+    }
+
+    void checkTime()
+    {
+        time -= Time.deltaTime;
+        if ( time <= 0 )
+        {
+            System.Random rnd = new System.Random();
+            int indx = rnd.Next(0, 3);
+
+            transform.position = v[indx];
+            time = 10f;
+        }
+    }
+
+    void MovePlayer()
+    {
+        if (Input.GetKey("left"))
+        {
+            transform.position -= new Vector3(carSpeed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey("right"))
+        {
+            transform.position += new Vector3(carSpeed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey("up"))
+        {
+            transform.position += new Vector3(0, carSpeed * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey("down"))
+        {
+            transform.position -= new Vector3(0, carSpeed * Time.deltaTime, 0);
+        }
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Vector3 currentPos = transform.position;
+        if (col.gameObject.name == "Tilemap")
+        {
+            transform.position -= (transform.position - currentPos);
+        }
+    }
+}
